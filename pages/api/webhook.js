fe33911168786607b2bda5000215e3898da77478
EnesCeman <enesceman1@gmail.com>
2024-01-04 09:53:37 +0100

    initial commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,46 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+
+const endpointSecret =
+  "whsec_b3cf3fa3534f31aeaebec7aa621e4d182ec590ea117f97a70bc4e143ffbf0650";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
